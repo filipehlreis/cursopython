8de4d616f1acb94c44b81959c7f4ab9ec5d84a06
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2021-11-05 22:58:19 -0300

    aula 187. Mensagens com Django Messages

--- sessao9_djangoAgenda/agenda/contatos/views.py
+++ sessao9_djangoAgenda/agenda/contatos/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
 from django.db.models import Q, Value
 from django.db.models.functions import Concat
+from django.contrib import messages
 
 
 def index(request):
@@ -34,7 +35,12 @@ def busca(request):
     termo = request.GET.get('termo')
 
     if termo is None:
-        raise Http404()
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'Campo termo não pode ficar vazio.'
+        )
+        return redirect('index')
 
     campos = Concat('nome', Value(' '), 'sobrenome')
 
@@ -44,7 +50,7 @@ def busca(request):
         Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
     )
 
-    #print(contatos.query)
+    # print(contatos.query)
     paginator = Paginator(contatos, 5)
 
     page = request.GET.get('p')
