c4191e7ebb3da1d8a167d550cb42b4fe17ec2d09
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2021-11-07 18:28:57 -0300

    aula 192: login, logout e dashboard

--- sessao9_djangoAgenda/agenda/accounts/views.py
+++ sessao9_djangoAgenda/agenda/accounts/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method != 'POST':
+        return render(request, 'accounts/login.html')
+
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+
+    user = auth.authenticate(request, username=usuario, password=senha)
+    if not user:
+        messages.error(request, 'Usuário ou senha inválidos.')
+        return render(request, 'accounts/login.html')
+    else:
+        auth.login(request, user)
+        messages.success(request, 'Voce fez login com sucesso.')
+        return redirect('dashboard')
 
 
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    auth.logout(request)
+    return redirect('index')
 
 
 def cadastro(request):
@@ -53,7 +68,7 @@ def cadastro(request):
         messages.error(request, 'Email já existe.')
         return render(request, 'accounts/cadastro.html')
 
-    messages.success(request, 'Registrado com sucesso.!\nAgora faça o login.')
+    messages.success(request, 'Registrado com sucesso!\nAgora faça o login.')
     user = User.objects.create_user(username=usuario,
                                     email=email,
                                     password=senha,
@@ -64,5 +79,6 @@ def cadastro(request):
     # return render(request, 'accounts/cadastro.html')
 
 
+@login_required(redirect_field_name='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
