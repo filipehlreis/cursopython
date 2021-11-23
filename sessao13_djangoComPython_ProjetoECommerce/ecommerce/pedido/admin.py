8ef56c0378eb7b1cffcc2b5d97e72159b53d6945
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2021-11-22 21:39:31 -0300

    aula 225: projeto ecommerce - criando models Pedido ItemPedido

--- sessao13_djangoComPython_ProjetoECommerce/ecommerce/pedido/admin.py
+++ sessao13_djangoComPython_ProjetoECommerce/ecommerce/pedido/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.db import models
+from pedido.models import ItemPedido, Pedido
 
-# Register your models here.
+
+class ItemPedidoInline(admin.TabularInline):
+    model = ItemPedido
+    extra = 1
+
+
+class PedidoAdmin(admin.ModelAdmin):
+    inlines = [
+        ItemPedidoInline
+    ]
+
+
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(ItemPedido)
