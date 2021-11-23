4c6f5e357a5a5e8210261fe7011dbe2d1770399e
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2021-11-22 22:39:30 -0300

    aula 227: projeto ecommerce - slug automatico e cadastro produtos

--- sessao13_djangoComPython_ProjetoECommerce/ecommerce/produto/admin.py
+++ sessao13_djangoComPython_ProjetoECommerce/ecommerce/produto/admin.py
@@ -2,15 +2,19 @@ from django.contrib import admin
 from django.db import models
 from .models import Produto, Variacao
 
+
 class VariacaoInline(admin.TabularInline):
     model = Variacao
     extra = 1
 
 
 class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'descricao_curta',
+               'get_preco_formatado', 'get_preco_promocional_formatado']
     inlines = [
         VariacaoInline
     ]
+
 
 admin.site.register(Produto, ProdutoAdmin)
 admin.site.register(Variacao)
