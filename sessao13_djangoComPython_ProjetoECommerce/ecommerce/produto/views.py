ed13e2c890ce7130ce5790ac5ab25e3358a3e0a3
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2021-11-25 22:19:46 -0300

    aula 230: projeto ecommerce - detalhes do produto

--- sessao13_djangoComPython_ProjetoECommerce/ecommerce/produto/views.py
+++ sessao13_djangoComPython_ProjetoECommerce/ecommerce/produto/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from . import models
 
@@ -12,9 +13,11 @@ class ListaProdutos(ListView):
     paginate_by = 10
 
 
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('DetalheProduto')
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AdicionarAoCarrinho(View):
