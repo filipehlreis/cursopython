b4386bae1085572b63ad6aebb927d28b756d5e69
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2021-12-06 22:32:01 -0300

    aula 249: assertions

--- sessao14_testesEintroducaoAoTDDnoPython_unittest/python_testes/calculadora.py
+++ sessao14_testesEintroducaoAoTDDnoPython_unittest/python_testes/calculadora.py
@@ -1,4 +1,6 @@
 def soma(x, y):
+    assert isinstance(x, (int, float)), 'x precisa ser int ou float'
+    assert isinstance(y, (int, float)), 'y precisa ser int ou float'
     return x + y
 
 
--- sessao14_testesEintroducaoAoTDDnoPython_unittest/python_testes/main.py
+++ sessao14_testesEintroducaoAoTDDnoPython_unittest/python_testes/main.py
@@ -3,4 +3,13 @@ from calculadora import soma
 print(soma(10, 20))
 print(soma(-10, 20))
 print(soma(1.5, 2.5))
+print(soma(15, 15))
+
+
+try:
+    print(soma(2, 's'))
+except AssertionError as e:
+    print(f'conta invalida: {e}')
+    print(f'conta invalida: {e}')
+
 
