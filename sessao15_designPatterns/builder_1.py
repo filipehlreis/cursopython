a3004c56d9c9aa335dfc9469772d5d3305588cf4
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2021-12-29 23:14:42 -0300

    aula 271: builder

---
+++ sessao15_designPatterns/builder_1.py
@@ -0,0 +1,115 @@
+"""
+Builder é um padrao de criacao que tem a intencao de separar a construcao de um
+objeto complexo da sua representacao, de modo que o mesmo processo de
+construcao possa criar diferentes representacoes.
+
+Builder te da a possibilidade de criar objetos passo-a-passo e isso já é
+possivel no python sem este padrao.
+
+Geralmente o builder aceita o encadeamento de metodos (method chaining).
+"""
+
+
+from abc import ABC, abstractmethod
+
+
+class StringReprMixin:
+    def __str__(self):
+        params = ', '.join(
+            [f'{k} = {v}' for k, v in self.__dict__.items()]
+        )
+        return f'{self.__class__.__name__}({params})'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class User(StringReprMixin):
+    def __init__(self):
+        self.firstname = None
+        self.lastname = None
+        self.age = None
+        self.phone_numbers = []
+        self.addresses = []
+
+
+class IUserBuilder(ABC):
+    @property
+    @abstractmethod
+    def result(self): pass
+
+    @abstractmethod
+    def add_firstname(self, firstname): pass
+
+    @abstractmethod
+    def add_lastname(self, lastname): pass
+
+    @abstractmethod
+    def add_age(self, age): pass
+
+    @abstractmethod
+    def add_phone(self, phone): pass
+
+    @abstractmethod
+    def add_address(self, address): pass
+
+
+class UserBuilder(IUserBuilder):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self._result = User()
+
+    @property
+    def result(self):
+        return_data = self._result
+        self.reset()
+        return return_data
+
+    def add_firstname(self, firstname):
+        self._result.firstname = firstname
+        return self
+
+    def add_lastname(self, lastname):
+        self._result.lastname = lastname
+        return self
+
+    def add_age(self, age):
+        self._result.age = age
+        return self
+
+    def add_phone(self, phone):
+        self._result.phone_numbers.append(phone)
+        return self
+
+    def add_address(self, address):
+        self._result.addresses.append(address)
+        return self
+
+
+class UserDirector():
+    def __init__(self, builder):
+        self._builder: UserBuilder = builder
+
+    def with_age(self, firstname, lastname, age):
+        self._builder\
+            .add_firstname(firstname)\
+            .add_lastname(lastname)\
+            .add_age(age)
+        return self._builder.result
+
+    def with_address(self, firstname, lastname, address):
+        self._builder.add_firstname(firstname)
+        self._builder.add_lastname(lastname)
+        self._builder.add_address(address)
+        return self._builder.result
+
+
+if __name__ == "__main__":
+    user_builder = UserBuilder()
+    user_director = UserDirector(user_builder)
+    user1 = user_director.with_age('Luiz', 'Otavio', 30)
+    user2 = user_director.with_address('Maria', 'Miranda  ', 'Avenida Brasil')
+    print(user1)
+    print(user2)
